30faf82b23400f4044a4e06ca8f089a3a49c3325
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-10-30 03:59:29 +0100

    Added SHA-0 algorithm

---
+++ Hashing/SHA/SHA0Algo/PaddingType.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleHashing.SHA.SHA0Algo
+{
+    internal enum PaddingType
+    {
+        Custom,
+        OneZeroFillAnd8BytesMessageLengthLittleEndian,
+        OneZeroFillAnd8BytesMessageLengthBigEndian,
+        OneZeroFillAnd16BytesMessageLengthBigEndian
+    }
+}
